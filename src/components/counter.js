df16544e0e1a420847bf3571e67811ce7b1911f0
randolphbailey <randolphbailey@gmail.com>
2019-05-01 06:07:38 -0500

    refactor some components to use function instead of class

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,9 +1,7 @@
 import React from "react";
 
-class Counter extends React.Component {
-  render() {
-    return <>{this.props.count}</>;
-  }
+function Counter(props) {
+  return <>{props.count}</>;
 }
 
 export default Counter;
